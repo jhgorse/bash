70b9c12bc2c91bbdbcfaf81a2ee6dee98eec163d
Joe Gorse <jhgorse@gmail.com>
2019-03-22 18:17:27 -0400

    local.sh <- .bashrc

---
+++ .config/bash/local.sh
@@ -0,0 +1,16 @@
+HISTCONTROL=ignoreboth:erasedups
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+
